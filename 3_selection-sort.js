2bfeb3b37810f041d305638b1b05eaafe63bf370
Alena <alyonatihomirova9@gmail.com>
2024-06-13 01:10:20 +0300

    Update

---
+++ 3_selection-sort.js
@@ -0,0 +1,25 @@
+const array = [5, 7, 8, 4, 3, 2, 1, 9, 6, 12, 18, -5, -7, -8, -4, -3, -2, -1, -9, -6, -12, -18]
+let count = 0
+
+function selectionSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        let minIndex = i
+        for (let j = i+1; j < array.length; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j
+            }
+            count += 1
+        }
+        
+        let temp = array[i]
+        array[i] = array[minIndex]
+        array[minIndex] = temp
+
+    }
+    return array
+}
+
+console.log(selectionSort(array))
+console.log("count = ", count)
+
+// Complexity: O(n^2) where n = the length of the array

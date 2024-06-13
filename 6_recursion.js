b86377c987baf4dce208fd63c275b18fd14941f0
Alena <alyonatihomirova9@gmail.com>
2024-06-14 00:17:30 +0300

    Update

--- 6_recursion.js
+++ 6_recursion.js
@@ -1,5 +1,5 @@
 const factorial = (n) => {
-    if (n === 1) { // The base case that stops the recursion
+    if (n === 1) { // The base case that stops the recursion. The factorial of 1 (1!) is 1
         return 1
     }
 
@@ -7,3 +7,21 @@ const factorial = (n) => {
 }
 
 console.log(factorial(9))
+
+
+
+
+
+
+
+// Fibonachi numbers: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610
+
+const fibonachi = (n) => {
+    if (n === 1 || n === 2) { // The base case that stops the recursion. The 1st and 2nd fibonachi number is 1
+        return 1
+    }
+
+    return fibonachi(n - 1) + fibonachi(n - 2)
+}
+
+console.log(fibonachi(8))

203ead146fd9e2e41bef53e922953c16dcf06e69
Alena <alyonatihomirova9@gmail.com>
2024-06-18 02:00:39 +0300

    Update

---
+++ 11_cash.js
@@ -0,0 +1,30 @@
+function cashFunction(fn) {
+    const cash = {}
+
+    return function(n) { // Closure
+        if (cash[n]) {
+            console.log("Using cash", cash[n])
+            return cash[n]
+        }
+
+        let result = fn(n)
+        console.log("Calculated by the function", result)
+        cash[n] = result
+        return result
+    }
+}
+
+function factorial(n) { // Iterative approach
+    let result = 1
+
+    while (n != 1) {
+        result *= n
+        n -= 1
+    }
+
+    return result
+}
+
+const cashFactorial = cashFunction(factorial)
+
+console.log(cashFactorial(9))
